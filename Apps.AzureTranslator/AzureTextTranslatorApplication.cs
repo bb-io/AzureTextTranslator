bdf853a58828789c65a40763bccc073bf713ec7b
bZverok <137277669+bZverok@users.noreply.github.com>
2024-04-09 13:47:24 +0300

    added categories

--- Apps.AzureTranslator/AzureTextTranslatorApplication.cs
+++ Apps.AzureTranslator/AzureTextTranslatorApplication.cs
@@ -1,9 +1,16 @@
 ﻿using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Metadata;
 
 namespace Apps.AzureTranslator;
 
-public class AzureTextTranslatorApplication : IApplication
+public class AzureTextTranslatorApplication : IApplication, ICategoryProvider
 {
+    public IEnumerable<ApplicationCategory> Categories
+    {
+        get => [ApplicationCategory.MachineTranslationAndMtqe, ApplicationCategory.AzureApps];
+        set { }
+    }
+    
     public string Name
     {
         get => "Azure Text Translator";
