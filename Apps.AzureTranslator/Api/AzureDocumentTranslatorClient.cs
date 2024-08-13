b2bd203c0f1d606b5aa3112d301e96f88990c0f5
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-08-13 13:57:02 +0300

    Added nuget for translating documents

---
+++ Apps.AzureTranslator/Api/AzureDocumentTranslatorClient.cs
@@ -0,0 +1,18 @@
+﻿using Apps.AzureTranslator.Constants;
+using Azure;
+using Azure.AI.Translation.Document;
+using Blackbird.Applications.Sdk.Common.Authentication;
+using Blackbird.Applications.Sdk.Utils.Extensions.Sdk;
+
+namespace Apps.AzureTranslator.Api;
+
+public class AzureDocumentTranslatorClient : DocumentTranslationClient
+{
+    public const string endpoint = "https://api.cognitive.microsofttranslator.com";
+    
+    public AzureDocumentTranslatorClient(AuthenticationCredentialsProvider[] creds) 
+        : base(new Uri(endpoint), new AzureKeyCredential(new(creds.Get(CredsNames.ApiKey).Value)))
+    {
+        
+    }
+}
